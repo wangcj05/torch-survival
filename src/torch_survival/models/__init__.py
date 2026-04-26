50b107264a0a7059d6a7b36d3ac8069804f293d7
Thomas Altstidl <10142068+taltstidl@users.noreply.github.com>
2026-04-26 15:43:28 +0200

    Comment out other models while they are being migrated

--- src/torch_survival/models/__init__.py
+++ src/torch_survival/models/__init__.py
@@ -1,5 +1,5 @@
 # alphabetical imports to make models more easily accessible
-from .deepsurv import DeepSurv, DeepSurvSearchSpace
-from .deephit import DeepHit
-from .rankdeepsurv import RankDeepSurv
-from .weibull import DeepWeiSurv
+from .deepsurv import DeepSurv
+# from .deephit import DeepHit
+# from .rankdeepsurv import RankDeepSurv
+# from .weibull import DeepWeiSurv
